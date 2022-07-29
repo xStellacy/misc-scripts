94b7170bc3a4018c4325923317bb7b6f20e1f6f3
Stellacy <87933277+xStellacy@users.noreply.github.com>
2022-07-29 12:18:01 -0700

    Add files via upload

---
+++ screenshot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+id=$(xdotool getactivewindow)
+xprop=$(xprop -id $id _NET_WM_NAME | awk -F \" {'print $2'})
+
+cd ~/Pictures/Screenshots && import "$(date '+%y-%m-%d %H:%M:%S') || ${xprop::228}.png"
