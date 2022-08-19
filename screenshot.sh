c53b56efd55f26e5b862c4f149c2f70bb034f60a
Stellacy <87933277+xStellacy@users.noreply.github.com>
2022-08-19 13:43:39 -0700

    Update screenshot.sh

--- screenshot.sh
+++ screenshot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 id=$(xdotool getactivewindow)
-xprop=$(xprop -id $id _NET_WM_NAME | awk -F \" {'print $2'})
+xprop=$(xprop -id $id _NET_WM_NAME | awk -F \" {'print $2'} | sed -e 's/\///g')
 
 cd ~/Pictures/Screenshots && import "$(date '+%y-%m-%d %H:%M:%S') || ${xprop::228}.png"
